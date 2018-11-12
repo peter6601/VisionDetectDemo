28907c8e2b154c08779e6f2a8744a10d69e423d0
Din Din <peterdindin@gmail.com>
2018-11-12 20:00:30 +0800

    Initial Commit

---
+++ VisionDetectDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionDetectDemo
+//
+//  Created by 丁暐哲 on 2018/11/12.
+//  Copyright © 2018 PeterDinDin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
